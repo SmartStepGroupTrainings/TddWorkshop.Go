e6f5594ac2a04c6503be6a4a69bcf8ec257a4634
Andrei Korzhevskii <akorzhevskii@akorzhevskii.local>
2015-07-30 17:35:49 +0300

    Use consts

--- casino/player_test.go
+++ casino/player_test.go
@@ -130,11 +130,12 @@ func (self *TestPlayerSuite) TestPlayer_Bet_DecreasesAvailableChips() {
 }
 
 func (self *TestPlayerSuite) TestPlayer_GetBetOn_SumsBets() {
+	anotherBet := Bet{Score: 1, Amount: 2}
 	self.p.BuyChips(10)
 
 	self.p.Bet(Bet{Score: 2, Amount: 1})
-	self.p.Bet(Bet{Score: 1, Amount: 2})
+	self.p.Bet(anotherBet)
 	self.p.Bet(Bet{Score: 2, Amount: 3})
 
-	assert.Equal(self.T(), 4, self.p.GetBetOn(2))
+	assert.Equal(self.T(), 1+3, self.p.GetBetOn(2))
 }
