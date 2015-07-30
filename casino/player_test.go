58ec9d24bc31d9169d6476d63eb2070688ef04ec
Anton Bevzuk <bevzuk@pisem.net>
2015-07-30 12:18:08 +0300

    Add player tests

--- casino/player_test.go
+++ casino/player_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
 func TestPlayer_ByDefault_HasNoChips(t *testing.T) {
@@ -20,11 +21,34 @@ func TestPlayer_CanBuyChips(t *testing.T) {
 	assert.Equal(t, 10, player.AvailableChips())
 }
 
-func TestPlayer_CanBetChips(t *testing.T) {
+func TestPlayer_CanBetChipsOn1(t *testing.T) {
 	player := NewPlayer()
 	player.BuyChips(10)
 
-	player.Bet(Bet{Score: 1, Amount: 2})
+	player.Bet(Bet{Amount: 2, Score: 1})
 
 	assert.Equal(t, 10-2, player.AvailableChips())
+}
+
+func (self *PlayerTest) TestPlayer_CanNotBetChipsOn7() {
+	self.player.BuyChips(10)
+
+	err := self.player.Bet(Bet{Amount: 2, Score: 7})
+
+	assert.Equal(self.T(), 10, self.player.AvailableChips())
+	assert.NotNil(self.T(), err)
+	assert.Equal(self.T(), "Bets on 1..6 only are allowed", err.Error())
+}
+
+type PlayerTest struct {
+	suite.Suite
+	player *Player
+}
+
+func (self *PlayerTest) SetupTest() {
+	self.player = NewPlayer()
+}
+
+func Test_RollDiceGame(t *testing.T) {
+	suite.Run(t, new(PlayerTest))
 }
