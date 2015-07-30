3c150b464a26e62c2fa3bfb6025c1c84926fb31d
Anatoly Panov <anatoly.panov@lazada.com>
2015-07-30 14:50:13 +0300

    added more tests for player

--- casino/player_test.go
+++ casino/player_test.go
@@ -14,18 +14,47 @@ func TestRun(t *testing.T) {
 	suite.Run(t, new(TestPlayerSuite))
 }
 
-func (suite *TestPlayerSuite) Test_NewPlayer_Create_Success() {
+func (suite *TestPlayerSuite) Test_CreateNewPlayer_Success() {
 	player := NewPlayer()
 
 	assert.False(suite.T(), player.IsInGame())
 	assert.Equal(suite.T(), 0, player.AvailableChips())
 }
 
-func (suite *TestPlayerSuite) Test_Player_JoinGame_Success() {
+func (suite *TestPlayerSuite) Test_JoinGame_Success() {
 	player := NewPlayer()
 	game := NewRollDiceGame()
 
 	player.Join(game)
 
 	assert.True(suite.T(), player.IsInGame())
+}
+
+func (suite *TestPlayerSuite) Test_JoinSimultaneouslySecondGame_Fail() {
+	player := NewPlayer()
+	game_one := NewRollDiceGame()
+	game_two := NewRollDiceGame()
+	player.Join(game_one)
+
+	err := player.Join(game_two)
+
+	assert.NotNil(suite.T(), err)
+}
+
+func (suite *TestPlayerSuite) Test_LeaveGame_Success() {
+	player := NewPlayer()
+	game := NewRollDiceGame()
+	player.Join(game)
+
+	player.Leave()
+
+	assert.False(suite.T(), player.IsInGame())
+}
+
+func (suite *TestPlayerSuite) Test_LeaveGameBeforeJoin_Fail() {
+	player := NewPlayer()
+
+	err := player.Leave()
+
+	assert.NotNil(suite.T(), err)
 }
