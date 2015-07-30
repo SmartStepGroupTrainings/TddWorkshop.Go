33be72ed2858d6cd8ca384c08c4c6d3ad9d6d9cc
Sergey Fedosenko <sergey.fedosenko@lazada.com>
2015-07-30 14:39:53 +0300

    Fix player_test.go

--- casino/player_test.go
+++ casino/player_test.go
@@ -2,69 +2,59 @@ package casino_new
 
 import (
 	"testing"
-    "github.com/stretchr/testify/assert"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestNewPlayerNotInGame_Leave_Error(t *testing.T) {
+func TestNewPlayerNotInGame_Leave_Fail(t *testing.T) {
 	player := NewPlayer()
 
 	err := player.Leave()
-    assert.NotNil(t, err, "Return value is not null")
-    assert.Equal(t, "Unable to leave the game before joining", err.Error())
+
+	assert.NotNil(t, err, "Return value is not null")
+	assert.Equal(t, "Unable to leave the game before joining", err.Error())
 }
 
 func TestNewPlayerInGame_Leave_Success(t *testing.T) {
-
 	player := NewPlayer()
 	player.Join(NewRollDiceGame())
 
 	err := player.Leave()
-    assert.Nil(t, err, "Player error is not null")
+	assert.Nil(t, err, "Player error is not null")
 }
 
-func TestPlayerIsInGame(t *testing.T) {
-	var isInGame bool
-
+func TestNewPlayer_IsInGame_Fail(t *testing.T) {
 	player := NewPlayer()
 
-	isInGame = player.IsInGame()
-	if isInGame {
-		t.Fatal("Player is in game")
-	}
-
-	player.Join(NewRollDiceGame())
-
-	isInGame = player.IsInGame()
-	if !isInGame {
-		t.Fatal("Player is not in game")
-	}
+	assert.Equal(t, false, player.IsInGame())
 }
-
-func TestPlayerBuyChips(t *testing.T) {
-	var err error
-
+func TestPlayerInGame_Join_Success(t *testing.T) {
 	player := NewPlayer()
 	player.Join(NewRollDiceGame())
 
-	err = player.BuyChips(-1)
-	if err == nil {
-		t.Fatal("Chips not valid")
-	}
-
-	err = player.BuyChips(22)
-	if err != nil {
-		t.Fatal("Chips not valid")
-	}
-
-	if player.AvailableChips() != 22 {
-		t.Fatal("Chips not apply")
-	}
+	assert.Equal(t, true, player.IsInGame())
 }
 
-func TestPlayerEmptyAvailableChips(t *testing.T) {
+func TestPlayerInGame_BuyChipsWithInvalidValue_Fail(t *testing.T) {
+	player := NewPlayer()
+	player.Join(NewRollDiceGame())
+
+	err := player.BuyChips(-1)
+	assert.NotNil(t, err, "Return value is not null")
+	assert.Equal(t, "Please buy positive amount", err.Error())
+}
+
+func TestPlayerInGame_BuyChipsWithValidValue_Success(t *testing.T) {
+	player := NewPlayer()
+	player.Join(NewRollDiceGame())
+
+	err := player.BuyChips(22)
+	assert.Nil(t, err, "Player error is not null")
+	assert.Equal(t, 22, player.AvailableChips())
+}
+
+func TestNewPlayer_AvailableChipsIsNull(t *testing.T) {
 	player := NewPlayer()
 
-	if player.AvailableChips() != 0 {
-		t.Fatal("Chips != 0")
-	}
+	assert.Equal(t, 0, player.AvailableChips())
 }
