1be54ad92c27390aed98bee8549bc5c5ad8ceaab
Sergey Fedosenko <sergey.fedosenko@lazada.com>
2015-07-30 14:58:44 +0300

    Fix player_test.go

--- casino/player_test.go
+++ casino/player_test.go
@@ -20,6 +20,7 @@ func TestNewPlayerInGame_Leave_Success(t *testing.T) {
 	player.Join(NewRollDiceGame())
 
 	err := player.Leave()
+
 	assert.Nil(t, err, "Player error is not null")
 }
 
@@ -40,6 +41,7 @@ func TestPlayerInGame_BuyChipsWithInvalidValue_Fail(t *testing.T) {
 	player.Join(NewRollDiceGame())
 
 	err := player.BuyChips(-1)
+
 	assert.NotNil(t, err, "Return value is not null")
 	assert.Equal(t, "Please buy positive amount", err.Error())
 }
@@ -49,6 +51,7 @@ func TestPlayerInGame_BuyChipsWithValidValue_Success(t *testing.T) {
 	player.Join(NewRollDiceGame())
 
 	err := player.BuyChips(22)
+
 	assert.Nil(t, err, "Player error is not null")
 	assert.Equal(t, 22, player.AvailableChips())
 }
@@ -65,6 +68,7 @@ func TestPlayerInGame_BetAmountMoreThanAvailable_Fail(t *testing.T) {
 	player.BuyChips(1)
 
 	err := player.Bet(Bet{Amount: 2, Score: 2})
+
 	assert.NotNil(t, err, "Return value is not null")
 	assert.Equal(t, "Unable to bet chips more than available", err.Error())
 
@@ -76,7 +80,18 @@ func TestPlayerInGame_BetScoreNotValid_Fail(t *testing.T) {
 	player.BuyChips(1)
 
 	err := player.Bet(Bet{Amount: 1, Score: 7})
+
 	assert.NotNil(t, err, "Return value is not null")
 	assert.Equal(t, "Bets on 1..6 only are allowed", err.Error())
+}
 
+func TestPlayerInGame_BetScoreValid_Success(t *testing.T) {
+	player := NewPlayer()
+	player.Join(NewRollDiceGame())
+	player.BuyChips(9)
+
+	_ = player.Bet(Bet{Amount: 2, Score: 5})
+
+	assert.Equal(t, 7, player.AvailableChips())
+	assert.Equal(t, 2, player.GetBetOn(5))
 }
