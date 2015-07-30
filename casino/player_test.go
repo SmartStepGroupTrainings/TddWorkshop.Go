af1f33fb45441c44058f3e80934c225ad515dee4
Sergey Fedosenko <sergey.fedosenko@lazada.com>
2015-07-30 15:24:10 +0300

    Fix player_test.go

--- casino/player_test.go
+++ casino/player_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewPlayerNotInGame_Leave_Fail(t *testing.T) {
+func TestPlayer_NotInGame_Leave_Fail(t *testing.T) {
 	player := NewPlayer()
 
 	err := player.Leave()
@@ -15,7 +15,7 @@ func TestNewPlayerNotInGame_Leave_Fail(t *testing.T) {
 	assert.Equal(t, "Unable to leave the game before joining", err.Error())
 }
 
-func TestNewPlayerInGame_Leave_Success(t *testing.T) {
+func TestPlayer_NotInGame_Leave_Success(t *testing.T) {
 	player := NewPlayer()
 	player.Join(NewRollDiceGame())
 
@@ -24,19 +24,19 @@ func TestNewPlayerInGame_Leave_Success(t *testing.T) {
 	assert.Nil(t, err, "Player error is not null")
 }
 
-func TestNewPlayer_IsInGame_Fail(t *testing.T) {
+func TestPlayer_NotInGame_IsInGame_Fail(t *testing.T) {
 	player := NewPlayer()
 
 	assert.Equal(t, false, player.IsInGame())
 }
-func TestPlayerInGame_Join_Success(t *testing.T) {
+func TestPlayer_InGame_Join_Success(t *testing.T) {
 	player := NewPlayer()
 	player.Join(NewRollDiceGame())
 
 	assert.Equal(t, true, player.IsInGame())
 }
 
-func TestPlayerInGame_BuyChipsWithInvalidValue_Fail(t *testing.T) {
+func TestPlayer_InGame_BuyChipsWithInvalidValue_Fail(t *testing.T) {
 	player := NewPlayer()
 	player.Join(NewRollDiceGame())
 
@@ -46,7 +46,7 @@ func TestPlayerInGame_BuyChipsWithInvalidValue_Fail(t *testing.T) {
 	assert.Equal(t, "Please buy positive amount", err.Error())
 }
 
-func TestPlayerInGame_BuyChipsWithValidValue_Success(t *testing.T) {
+func TestPlayer_InGame_BuyChipsWithValidValue_Success(t *testing.T) {
 	player := NewPlayer()
 	player.Join(NewRollDiceGame())
 
@@ -56,13 +56,13 @@ func TestPlayerInGame_BuyChipsWithValidValue_Success(t *testing.T) {
 	assert.Equal(t, 22, player.AvailableChips())
 }
 
-func TestNewPlayer_AvailableChipsIsNull(t *testing.T) {
+func TestPlayer_NotInGame_HasAvailableChipsIsZero(t *testing.T) {
 	player := NewPlayer()
 
 	assert.Equal(t, 0, player.AvailableChips())
 }
 
-func TestPlayerInGame_BetAmountMoreThanAvailable_Fail(t *testing.T) {
+func TestPlayer_InGame_BetAmountMoreThanAvailable_Fail(t *testing.T) {
 	player := NewPlayer()
 	player.Join(NewRollDiceGame())
 	player.BuyChips(1)
@@ -74,7 +74,7 @@ func TestPlayerInGame_BetAmountMoreThanAvailable_Fail(t *testing.T) {
 
 }
 
-func TestPlayerInGame_BetScoreNotValid_Fail(t *testing.T) {
+func TestPlayer_InGame_BetScoreNotValid_Fail(t *testing.T) {
 	player := NewPlayer()
 	player.Join(NewRollDiceGame())
 	player.BuyChips(1)
@@ -85,13 +85,13 @@ func TestPlayerInGame_BetScoreNotValid_Fail(t *testing.T) {
 	assert.Equal(t, "Bets on 1..6 only are allowed", err.Error())
 }
 
-func TestPlayerInGame_BetScoreValid_Success(t *testing.T) {
+func TestPlayer_InGame_BetScoreValid_Success(t *testing.T) {
 	player := NewPlayer()
 	player.Join(NewRollDiceGame())
-	player.BuyChips(9)
+	player.BuyChips(1)
 
-	_ = player.Bet(Bet{Amount: 2, Score: 5})
+	_ = player.Bet(Bet{Amount: 1, Score: 5})
 
-	assert.Equal(t, 7, player.AvailableChips())
-	assert.Equal(t, 2, player.GetBetOn(5))
+	assert.Equal(t, 1-1, player.AvailableChips())
+	assert.Equal(t, 0+1, player.GetBetOn(5))
 }
