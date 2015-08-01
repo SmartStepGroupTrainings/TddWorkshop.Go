fef2b0a8ea6252fe7f05c6ccd8e1dac9d0334a3c
Anton Bevzuk <bevzuk@pisem.net>
2015-08-01 10:51:18 +0300

    First TDD test

--- casino/player_test.go
+++ casino/player_test.go
@@ -1,68 +1,22 @@
-package casino_new
+package casino
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/mock"
-	"github.com/stretchr/testify/suite"
 )
 
-func TestPlayer_ByDefault_HasNoChips(t *testing.T) {
-	player := NewPlayer()
+func Test_PlayerByDefaultNotInGame(t *testing.T) {
+	game := Game{}
+	player := Player{}
 
-	assert.Equal(t, 0, player.AvailableChips())
+	assert.False(t, player.IsIn(game))
 }
 
-func TestPlayer_CanBuyChips(t *testing.T) {
-	player := NewPlayer()
+type Player struct{}
 
-	player.BuyChips(10)
-
-	assert.Equal(t, 10, player.AvailableChips())
+func (self *Player) IsIn(game Game) bool {
+	return false
 }
 
-func TestPlayer_CanBetChipsOn1(t *testing.T) {
-	player := NewPlayer()
-	player.BuyChips(10)
-
-	player.Bet(Bet{Amount: 2, Score: 1})
-
-	assert.Equal(t, 10-2, player.AvailableChips())
-}
-
-func (self *PlayerTest) TestPlayer_CanNotBetChipsOn7() {
-	self.player.BuyChips(10)
-
-	err := self.player.Bet(Bet{Amount: 2, Score: 7})
-
-	assert.Equal(self.T(), 10, self.player.AvailableChips())
-	assert.NotNil(self.T(), err)
-	assert.Equal(self.T(), "Bets on 1..6 only are allowed", err.Error())
-}
-
-type PlayerTest struct {
-	suite.Suite
-	player *Player
-	dice   *DiceStub
-	game   *RollDiceGame
-}
-
-func (self *PlayerTest) SetupTest() {
-	self.player = NewPlayer()
-	self.dice = new(DiceStub)
-	self.game = NewRollDiceGame(self.dice)
-}
-
-func Test_RollDiceGame(t *testing.T) {
-	suite.Run(t, new(PlayerTest))
-}
-
-type DiceStub struct {
-	mock.Mock
-}
-
-func (self DiceStub) Roll() int {
-	args := self.Called()
-	return args.Int(0)
-}
+type Game struct{}
