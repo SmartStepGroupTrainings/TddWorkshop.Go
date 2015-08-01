a33c28ce1be72d374d67229bcf587b9c92bab4ff
Denis Glazkov <denis.glazkov@lazada.com>
2015-08-01 12:21:39 +0300

    player can not join twice to game

--- casino/player.go
+++ casino/player.go
@@ -1,15 +1,25 @@
 package casino
+import "github.com/dropbox/godropbox/errors"
+
 
 type Player struct {
 	game *Game
 }
 
-func (player *Player) Join(game *Game) {
+func (player *Player) Join(game *Game) error {
+	if player.IsInGame() {
+		return errors.New("Player not in game")
+	}
 	player.game = game
+	return nil
 }
 
-func (player *Player) LeaveGame() {
+func (player *Player) LeaveGame() error {
+	if !player.IsInGame() {
+		return errors.New("Player not in game")
+	}
 	player.game = nil
+	return nil
 }
 
 func (player *Player) IsInGame() bool {
--- casino/player_test.go
+++ casino/player_test.go
@@ -34,4 +34,24 @@ func (test *GameTest) Test_Player_Leave_Success() {
 	test.False(player.IsInGame())
 }
 
+func (test *GameTest) Test_PlayerNotInGame_LeaveGame_Fail() {
+	player := &Player{}
+
+	err := player.LeaveGame()
+
+	test.NotNil(err)
+}
+
+
+func (test *GameTest) Test_Player_JoinTwiceInGame_Fail() {
+	player := &Player{}
+	game1 := &Game{}
+	game2 := &Game{}
+	player.Join(game1)
+
+	err := player.Join(game2)
+
+	test.NotNil(err)
+}
+
 
