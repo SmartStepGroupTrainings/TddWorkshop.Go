6b148973993564d21db99802d36b59fee0f47b3b
Alexander Grafov <alexander.grafov@lazada.com>
2015-08-01 11:55:47 +0300

    test leave game

--- casino/casino_test.go
+++ casino/casino_test.go
@@ -1,21 +1,32 @@
 package casino
 
 import (
-    "github.com/stretchr/testify/assert"
-    "testing"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestPlayer_ByDefault_IsNotInGame(t *testing.T) {
-    player := Player{}
+	player := Player{}
 
-    assert.False(t, player.IsInGame())
+	assert.False(t, player.IsInGame())
 }
 
 func TestPlayer_ByDefault_CanJoinGame(t *testing.T) {
-    player := Player{}
-    game := Game{}
+	player := Player{}
+	game := Game{}
 
-    player.Join(game)
+	player.Join(game)
 
-    assert.True(t, player.IsInGame())
+	assert.True(t, player.IsInGame())
+}
+
+func TestPlayer_InGame_CanLeaveGame(t *testing.T) {
+	player := Player{}
+	game := Game{}
+	player.Join(game)
+
+	player.Leave(game)
+
+	assert.False(t, player.IsInGame())
 }
--- casino/player.go
+++ casino/player.go
@@ -1,13 +1,17 @@
 package casino
 
 type Player struct {
-    isInGame bool
+	isInGame bool
 }
 
 func (p *Player) IsInGame() bool {
-    return p.isInGame
+	return p.isInGame
 }
 
 func (p *Player) Join(game Game) {
-    p.isInGame = true
+	p.isInGame = true
+}
+
+func (p *Player) Leave(game Game) {
+	p.isInGame = false
 }
